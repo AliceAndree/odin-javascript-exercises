7b199959b68b0ebd60af7ef50cc5242939d3040e
AliceAndree <thiel.alice@gmail.com>
2024-12-01 13:21:17 +0100

    Succeeded first test of 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, valueToRemove) {
+  let valueToRemoveIndex = array.indexOf(valueToRemove);
+  array.splice(valueToRemoveIndex, 1);
+  return array;
 };
 
 // Do not edit below this line
