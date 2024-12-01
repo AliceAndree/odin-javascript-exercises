334a6abaa8f5aade825333ed24dea5bc5cac18eb
AliceAndree <thiel.alice@gmail.com>
2024-12-01 15:41:41 +0100

    Succeeded at all test for 05_sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  const array = [];
+  let lowerNumber = Math.min(firstNumber, secondNumber);
+  let upperNumber = Math.max(firstNumber, secondNumber);
 
+  if (Math.sign(firstNumber) === -1 || Math.sign(secondNumber) === -1) {
+    return "ERROR";
+  } else if (
+    !Number.isInteger(firstNumber) ||
+    !Number.isInteger(secondNumber)
+  ) {
+    return "ERROR";
+  } else if (
+    parseInt(firstNumber) !== firstNumber ||
+    parseInt(secondNumber) !== secondNumber
+  ) {
+    return "ERROR";
+  } else {
+    for (let i = lowerNumber; i <= upperNumber; i++) {
+      array.push(i);
+    }
+
+    let sum = 0;
+    for (let i of array) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
