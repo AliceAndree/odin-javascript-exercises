d0590bb6638eaea9d453d1c232b827754eaab751
AliceAndree <thiel.alice@gmail.com>
2024-12-01 16:43:13 +0100

    Succeeded at all 07_tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let fahrenheitToCelsius = (fahrenheit - 32) / 1.8;
+  let roundedCelsius = Math.round(fahrenheitToCelsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let celsiusToFahrenheit = celsius * 1.8 + 32;
+  let roundedFahrenheit = Math.round(celsiusToFahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
