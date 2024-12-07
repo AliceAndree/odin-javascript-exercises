f4f4f902172f846cfd496531400bb1b80af2e522
AliceAndree <thiel.alice@gmail.com>
2024-12-07 17:42:44 +0100

    Succeeded at all 08_calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+  const sum = firstNumber + secondNumber;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+  const subtraction = firstNumber - secondNumber;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const totalSum = array.reduce((total, num) => total + num, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const totalProduct = array.reduce((total, num) => total * num);
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function (numberOne, numberTwo) {
+  const totalPower = Math.pow(numberOne, numberTwo);
+  return totalPower;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
