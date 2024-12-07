67f9e795ac9ec1ab714956cbda15416edd7bda0f
AliceAndree <thiel.alice@gmail.com>
2024-12-07 21:26:18 +0100

    Succeeded at all 09_palindromes tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphabetical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const filteredString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphabetical.includes(character))
+    .join("");
+  const reversedString = filteredString.split("").reverse().join("");
 
+  if (filteredString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
