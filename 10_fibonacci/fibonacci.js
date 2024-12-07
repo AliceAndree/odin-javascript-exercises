484b749555573e14c8f56ea4bce8c437477ff655
AliceAndree <thiel.alice@gmail.com>
2024-12-07 22:42:15 +0100

    Succeeded at all 10_fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (thNumber) {
+  if (Math.sign(thNumber) === -1) {
+    return "OOPS";
+  }
+  const fib = [0, 1];
+  for (let i = 2; i <= thNumber; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[thNumber];
 };
 
 // Do not edit below this line
