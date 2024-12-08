c928e54a5f36a5fe83b67c73f318d7deb6f39843
AliceAndree <thiel.alice@gmail.com>
2024-12-08 16:26:59 +0100

    Succeeded at all 12_findTheOldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayOfPeople) {
+  let ages = [];
+  arrayOfPeople.forEach((people) => {
+    if ("yearOfDeath" in people) {
+      ages.push(people.yearOfDeath - people.yearOfBirth);
+    } else {
+      const currentYear = new Date().getFullYear();
+      ages.push(currentYear - people.yearOfBirth);
+    }
+  });
 
+  const biggestAge = Math.max(...ages);
+  const indexOfBiggestAge = ages.indexOf(biggestAge);
+  const oldestPerson = arrayOfPeople[indexOfBiggestAge];
+  return oldestPerson;
 };
 
 // Do not edit below this line
